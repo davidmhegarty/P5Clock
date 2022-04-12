a4c2cd5ae6fc3525df59bdcba0244f345e668203
David Hegarty <david.m.hegarty@gmail.com>
2022-04-12 01:20:37 +0100

    refactored with functions

--- sketch.js
+++ sketch.js
@@ -2,8 +2,6 @@
 
 https://www.youtube.com/watch?v=E4RyStef-gY&ab_channel=TheCodingTrain
 
-
-
 */
 function setup() {
     createCanvas(900, 600);
@@ -16,52 +14,54 @@ function draw() {
     let mn = minute();
     let sec = second();
 
-    hrArc = map(hr % 12, 0, 12, 0, 360);
-    mnArc = map(mn, 0, 60, 0, 360);
-    secArc = map(sec, 0, 60, 0, 360);
+    drawNeonClock(hr, mn, sec);
 
-    noFill();
+    drawDigitalDisplay(hr, mn, sec, 400, 500);
 
+}
 
+function drawDigitalDisplay(_hr, _mn, _sec, _x, _y) {
     push();
     fill(255);
     noStroke();
     textSize(30);
     textAlign(CENTER);
-    text(hr + ':' + mn + ':' + sec, 200, 400);
+    text(nf(_hr,2) + ':' + nf(_mn,2) + ':' + nf(_sec,2), _x, _y);
     pop();
+}
 
-    translate(200, 200);
-    rotate(-90);
+function drawNeonClock(_hr, _mn, _sec) {
+    
+    hrArc = map(_hr % 12, 0, 12, 0, 330) + 
+            map(_mn, 0, 60, 0, 29.5) + 
+            map(_sec, 0, 60, 0, 0.5);
+    mnArc = map(_mn, 0, 60, 0, 354) + map(_sec, 0, 60, 0, 6);
+    secArc = map(_sec, 0, 60, 0, 360);
 
     push();
+    noFill();
+    translate(400, 300);
+    rotate(-90);
     stroke(255);
     strokeWeight(4);
     ellipse(0, 0, 300, 300);
-    pop();
+    
+    // seconds arc and hand
+    drawArc(230, 100, secArc, color(255,173,0));
+    // minutes arc and hand
+    drawArc(250, 75, mnArc, color(68,214,44));
+    // hours arc and hand
+    drawArc(270, 50, hrArc, color(199,36,177));
 
+    pop();
+}
+
+function drawArc(_width, _lineLength, _arc, _colour) {
     push();
-    stroke(255,173,0);
+    stroke(_colour);
     strokeWeight(5);
-    arc(0, 0, 230, 230, 0, secArc);
-    rotate(secArc);
-    line(0, 0, 100, 0);
+    arc(0, 0, _width, _width, 0, _arc);
+    rotate(_arc);
+    line(0, 0, _lineLength, 0);
     pop();
-
-    push();
-    stroke(68,214,44);
-    strokeWeight(5);
-    arc(0, 0, 250, 250, 0, mnArc);
-    rotate(mnArc);
-    line(0, 0, 75, 0);
-    pop();
-
-    push();
-    stroke(199,36,177);
-    strokeWeight(5);
-    arc(0, 0, 270, 270, 0, hrArc);
-    rotate(hrArc);
-    line(0, 0, 50, 0);
-    pop();
-
 }
